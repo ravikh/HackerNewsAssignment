acb4d5f91b2b7fefe304f606bf153e6baf388cbd
Ravi Khandelwal <ravkhand0@publicisgroupe.net>
2020-04-11 23:37:52 +0530

    travis ci integration

--- app/utils/tests/serviceUtil.test.js
+++ app/utils/tests/serviceUtil.test.js
@@ -4,29 +4,33 @@ import fetch from '../serviceUtil';
 jest.mock('axios');
 
 describe('test service util', () => {
-    test('fetches successfully data from an API', async () => {
-        const data = {
-            data: {
-                hits: [
-                    {
-                        objectID: '1',
-                        title: 'a',
-                    },
-                    {
-                        objectID: '2',
-                        title: 'b',
-                    },
-                ],
-            },
-        };
-        axios.get.mockImplementationOnce(() => Promise.resolve(data));
-        await expect(fetch('url endpoint')).resolves.toEqual(data);
-    });
+  test('fetches successfully data from an API', async () => {
+    const data = {
+      data: {
+        hits: [
+          {
+            objectID: '1',
+            title: 'a',
+          },
+          {
+            objectID: '2',
+            title: 'b',
+          },
+        ],
+      },
+    };
+    // eslint-disable-next-line compat/compat
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+    await expect(fetch('url endpoint')).resolves.toEqual(data);
+  });
 
-    test('fetches erroneously data from an API', async () => {
-        const errorMessage = 'Network Error';
-        axios.get.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+  test('fetches erroneously data from an API', async () => {
+    const errorMessage = 'Network Error';
+    axios.get.mockImplementationOnce(() =>
+      // eslint-disable-next-line compat/compat
+      Promise.reject(new Error(errorMessage))
+    );
 
-        await expect(fetch('url endpoint')).rejects.toThrow(errorMessage);
-    });
+    await expect(fetch('url endpoint')).rejects.toThrow(errorMessage);
+  });
 });
